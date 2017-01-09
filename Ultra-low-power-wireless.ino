7931db7aeee45baae6df5cd2c5712118c05370cc
Ashish Jayan <ashish@linux.com>
2017-01-08 20:23:17 -0600

    Add files via upload

---
+++ Ultra-low-power-wireless.ino
@@ -0,0 +1,86 @@
+// Library
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+// WiFi settings
+
+String apiKey = "50BLPEPFF1942BRQ";
+const char ssid[] = "Wi believe i can FI";
+const char password[] = "blackspider100";
+// Time to sleep (in seconds):
+const int sleepTimeS = (120000000 * 30) ;//1hr
+// Host
+const char* host = "dweet.io";
+
+const char* server = "api.thingspeak.com";
+#define DHTPIN 5 // what pin we’re connected to
+
+DHT dht(DHTPIN, DHT11, 15);
+WiFiClient client;
+
+void setup() 
+{
+  // Serial
+  Serial.begin(115200);
+  Serial.println("ESP8266 Booting");
+  
+  // Connect to WiFi
+WiFi.begin(ssid, password);
+
+Serial.println();
+Serial.println();
+Serial.print("Connecting to ");
+Serial.println(ssid);
+
+WiFi.begin(ssid, password);
+
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi working");
+
+
+float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Cant read from temp sensor");
+    return;
+  }
+
+  if (client.connect(server, 80)) { // "184.106.153.149" or api.thingspeak.com
+    String postStr = apiKey;
+    postStr += "&field1=";
+    postStr += String(t);
+    postStr += "&field2=";
+    postStr += String(h);
+    postStr += "\r\n\r\n";
+
+    client.print("POST /update HTTP/1.1\n");
+    client.print("Host: api.thingspeak.com\n");
+    client.print("Connection: close\n");
+    client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+    client.print("Content-Type: application/x-www-form-urlencoded\n");
+    client.print("Content-Length: ");
+    client.print(postStr.length());
+    client.print("\n\n");
+    client.print(postStr);
+
+    Serial.print("Temperature: ");
+    Serial.print(t);
+    Serial.print(" degrees Celcius Humidity: ");
+    Serial.print(h);
+    Serial.println("% send to Thingspeak");
+  }
+  client.stop();
+
+
+  Serial.println("ESP8266 going to deep sleep mode");
+  ESP.deepSleep(sleepTimeS);
+  
+}
+void loop() 
+{
+
+  
+}
